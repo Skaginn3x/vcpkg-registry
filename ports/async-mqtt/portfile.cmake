aca3aa5a33cc8a37d844df7cd5d0e85762ef59d0
magni <magnimar@skaginn3x.com>
2023-07-05 09:03:18 +0000

    adding async_mqtt

---
+++ ports/async-mqtt/portfile.cmake
@@ -0,0 +1,27 @@
+set(VCPKG_BUILD_TYPE release) #header-only
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO redboltz/async_mqtt
+    REF ad5910358e0bd3c897a9c056d3c3be52c2a078be
+    SHA512 0
+    HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DASYNC_MQTT_BUILD_TOOLS=OFF
+        -DASYNC_MQTT_BUILD_EXAMPLES=OFF
+        -DASYNC_MQTT_BUILD_UNIT_TESTS=OFF
+        -DASYNC_MQTT_BUILD_SYSTEM_TESTS=OFF
+        -DCMAKE_DISABLE_FIND_PACKAGE_Doxygen=ON
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(PACKAGE_NAME async_mqtt_iface CONFIG_PATH "lib/cmake/async_mqtt_iface")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
