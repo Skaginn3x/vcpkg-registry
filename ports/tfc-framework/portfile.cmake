89ce653027901948167f08b551c70988f7180838
magni <magnimar@skaginn3x.com>
2023-07-31 13:28:30 +0000

    looking at sparkplug fix

--- ports/tfc-framework/portfile.cmake
+++ ports/tfc-framework/portfile.cmake
@@ -1,9 +1,9 @@
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO skaginn3x/framework
-        REF 09328ebe573bfd083db09c7f6fc61ff0f1b569f9
-        SHA512 83e160657300aab2208b99989565c630425f0669c8338a0080737eda3c734b51e93d45b8aa6ab84535895402894421011c04831d4339215203e3fd1ff8bd5a3b
-        HEAD_REF main
+        REF 0f11c347c86ec0b3a548b339f0748bcccf5367b2
+        SHA512 0
+        HEAD_REF protobuf_fix
 )
 
 if ("build-exes" IN_LIST FEATURES)
